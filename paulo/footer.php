850c9005f7c641b03ce7f4132277c704f4a22c3c
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-06 12:55:41 +0200

    Update

--- paulo/footer.php
+++ paulo/footer.php
@@ -1,5 +1,16 @@
-<a href="http://www.youtube.com"><img src="images/youtube.png" width="25" height="25"></a>
-    <a href="http://www.twitter.com"><img src="images/twitter.png" width="25" height="25"></a>
-    <a href="http://www.facebook.com"><img src="images/facebook.png" width="25" height="25"></a>
-    <a href="mailto:retroflix@flix.com"><img src="images/email.png" width="25" height="25"></a>
-    <a href="http://www.instagram.com"><img src="/retroflix/images/instagram.png" width="25" height="25"></a>
+<footer class="footer mt-auto py-3 text-center">
+  <div class="row">
+    <div class="col-12 col-md-6">
+      <p>Retroflix © 2021 Created with Visual Studio Code.</p>
+    </div>
+    <div class="col-12 col-md-6">
+      <ul class="nav justify-content-center">
+          <li class="nav-item"><a class="nav-link text-white" href="http://www.youtube.com"><i class="fab fa-youtube-square"></i></a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="http://www.twitter.com"><i class="fab fa-twitter-square"></i></a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="http://www.facebook.com"><i class="fab fa-facebook-square"></i></a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="mailto:retroflix@flix.com"><i class="fas fa-envelope-square"></i></a></a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="http://www.instagram.com"><i class="fab fa-instagram-square"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
--- paulo/header.php
+++ paulo/header.php
@@ -1,35 +1,32 @@
-<!-- <section class="navigation-bar">
-        <h1 id="logo"><a href="index.php">RETROFLIX</h1></a>
-        <a href="browse.php" id="browse">BROWSE MOVIES</a>
-        <input type="text" placeholder="Search...">
-        <a href="users/create-account.php"><button type="button">INSCRIPTION</button></a>
-        <a href="users/sign-in.php"><button type="button" href="users/sign-in.php">CONNEXION</button></a>
-    </section> -->
-    <nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">RETROFLIX</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="browse.php">BROWSE MOVIES</a>
-        </li>
-      </ul>
-      <form action="search.php" method="get" class="d-flex">
-      <input class="form-control me-2" type="search" name="keyword" placeholder="Recherche par Titre ou Genre..." aria-label="search">
-                        <button class="btn btn-secondary" type="submit" name="search"><i class="fas fa-search"></i></button>
-      </form>
-      <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/users/create-account.php">S'inscrire</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/users/sign-in.php">Se connecter</a>
-        </li>
+<header>
+  <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container">
+      <a class="navbar-brand text-white" href="/index.php">RETROFLIX</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="/index.php">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="/base/browse.php">Explorer les films</a>
+          </li>
+        </ul>
+        <form class="d-flex" action="/base/search.php" method="get">
+          <input class="form-control me-2" type="search" name="keyword" placeholder="Recherche par Titre ou Genre..." aria-label="search">
+          <button class="btn btn-secondary" type="submit" name="search"><i class="fas fa-search"></i></button>
+        </form>
+        <ul class="navbar-nav mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="/users/create-account.php">S'inscrire</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="/users/sign-in.php">Se connecter</a>
+          </li>
+        </ul>        
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</header>
