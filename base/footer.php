283bf8450fd3004cf82c1206c7ed883a5373711e
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-06 17:29:22 +0200

    Fix Footer icons

--- base/footer.php
+++ base/footer.php
@@ -5,12 +5,12 @@
     </div>
     <div class="col-12 col-md-6">
       <ul class="nav justify-content-center">
-          <li class="nav-item"><a href="https://github.com/paulolvsn/retroflix"><i class="fab fa-github-square"></i></a></li>
-          <li class="nav-item"><a class="nav-link text-white" href="http://www.youtube.com"><i class="fab fa-youtube-square"></i></a></li>
-          <li class="nav-item"><a class="nav-link text-white" href="http://www.twitter.com"><i class="fab fa-twitter-square"></i></a></li>
-          <li class="nav-item"><a class="nav-link text-white" href="http://www.facebook.com"><i class="fab fa-facebook-square"></i></a></li>
-          <li class="nav-item"><a class="nav-link text-white" href="mailto:retroflix@flix.com"><i class="fas fa-envelope-square"></i></a></a></li>
-          <li class="nav-item"><a class="nav-link text-white" href="http://www.instagram.com"><i class="fab fa-instagram-square"></i></a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="https://github.com/paulolvsn/retroflix"><i class="fab fa-github-square"></i></a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="http://www.youtube.com"><i class="fab fa-youtube-square"></i></a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="http://www.twitter.com"><i class="fab fa-twitter-square"></i></a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="http://www.facebook.com"><i class="fab fa-facebook-square"></i></a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="mailto:retroflix@flix.com"><i class="fas fa-envelope-square"></i></a></a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="http://www.instagram.com"><i class="fab fa-instagram-square"></i></a></li>
       </ul>
     </div>
   </div>
