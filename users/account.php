37e2fd25f603c2cae11d8a086a171d802aa3ee02
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-09 15:34:42 +0200

    Fix account.php

--- users/account.php
+++ users/account.php
@@ -3,88 +3,88 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-  <?php
-    include("../base/head.php");
-  ?>
+    <?php
+        include("../base/head.php");
+    ?>
   <body class="bg-dark text-white">
-      <?php
-        include("../base/header.php");
-      ?> 
-      <main class="container">
         <?php
-          include_once('../users/connect-to-bdd.php');
-          function changesMess(){
-            if(isset($_GET['changes'])){
-              if($_GET['changes']=="ok"){
-                $message = "<p>changes were made successfully!</p>";
-                return $message;
-              }else{
-                $message = "<p>Something went wrong password are incorrect, please try again.</p>";
-                return $message;
-              }
-            }
-          }
-          $pseudo = $_SESSION['pseudo'];
-          $reponse = $bdd->query("SELECT avatar, email, pseudo FROM users WHERE pseudo='$pseudo'");
-          $data = $reponse->fetch();
-          $_SESSION['avatar']= $data['avatar'];
-          $avatar = $data['avatar'];
-          if(isset($_SESSION['admin'])) {
-            if($_SESSION['admin'] == 1) {
-              echo "
-                <div role='tabpanel'>
-                <!-- List group -->
-                  <div class='list-group list-group-horizontal' id='userPanel' role='tablist'>
-                    <a class='list-group-item list-group-item-action active' id='btnAdminPanel' data-bs-toggle='list' href='#admin' role='tab'>Admin Panel</a>
-                    <a class='list-group-item list-group-item-action' id='btnUserPanel' data-bs-toggle='list' href='#user' role='tab'>User Panel</a>
-                  </div>
-                <!-- Tab panes -->
-                <div class='tab-content'>
-                  <div class='tab-pane' id='user' role='tabpanel'>
-              ";
-            }
-          }
-          ?>
-              <div class="container" >
-                <div class="row mt-5">
-                    <div class="mt-5 col-3 offset-4 border border-dark border-2 rounded shadow p-3 mb-5 bg-body rounded text-dark">      
-                      <form method='POST' enctype=multipart/form-data action="user-save-changes.php">
-                        <div class="card form-control" >
-                          <h3> <?php echo "Welcome " . $pseudo;   ?></h3>
-                          <?php  echo  '<img id="avatarDisplay" src="'.$avatar.'" alt="avatar"><br>'; ?>    
-                          <div class="card-body">
-                            <input class="form-control" id=avatar type='file' name='file'/>
-                          </div>
-                        </div>              
-                        <label for="oldPass">old password</label><br>
-                        <input type="password" name="oldPass"><br>
-                        <label for="newPass">new password</label><br>
-                        <input type="password" name="newPass"><br>
-                        <label for="newPass2">new password Confirmation</label><br>
-                        <input type="password" name="newPass2"><br>
-                        <div class="d-grid gap-2 mt-3">    
-                        <input class="btn btn-outline-primary btn-block"  type="submit" value="Save Changes"><br>
-                        </div> 
-                        <div class="form-text"><?php echo changesMess(); ?></div>
-                      </form>
-                    </div>
-                </div>
+            include("../base/header.php");
+        ?> 
+        <main class="container">
             <?php
-              if(isset($_SESSION['admin'])) {
-                if($_SESSION['admin'] == 1) {
-                  echo "
-                      </div>
-                    </div>
-                    <div class='tab-pane active' id='admin' role='tabpanel'>                
-                  ";
-                  include "../users/connect-to-bdd.php"; // open database
-                  include "../key.php"; // load API key
-                  include "../admin/adminPanel.php";
-                  echo "
-                    </div>
-                  ";
+                include_once('../users/connect-to-bdd.php');
+                function changesMess() {
+                    if(isset($_GET['changes'])) {
+                        if($_GET['changes']=="ok") {
+                            $message = "<p>changes were made successfully!</p>";
+                            return $message;
+                        } else {
+                            $message = "<p>Something went wrong password are incorrect, please try again.</p>";
+                            return $message;
+                        }
+                    }
                 }
-              }
+                $pseudo = $_SESSION['pseudo'];
+                $reponse = $bdd->query("SELECT avatar, email, pseudo FROM users WHERE pseudo='$pseudo'");
+                $data = $reponse->fetch();
+                $_SESSION['avatar']= $data['avatar'];
+                $avatar = $data['avatar'];
+                if(isset($_SESSION['admin'])) {
+                    if($_SESSION['admin'] == 1) {
+                    echo "
+                        <div role='tabpanel'>
+                        <!-- List group -->
+                        <div class='list-group list-group-horizontal' id='userPanel' role='tablist'>
+                            <a class='list-group-item list-group-item-action active' id='btnAdminPanel' data-bs-toggle='list' href='#admin' role='tab'>Admin Panel</a>
+                            <a class='list-group-item list-group-item-action' id='btnUserPanel' data-bs-toggle='list' href='#user' role='tab'>User Panel</a>
+                        </div>
+                        <!-- Tab panes -->
+                        <div class='tab-content'>
+                        <div class='tab-pane' id='user' role='tabpanel'>
+                    ";
+                    }
+                }
+            ?>
+            <div class="container" >
+            <div class="row d-flex justify-content-center mt-5">
+                <div class="col-auto border border-light border-1 rounded shadow my-5 p-3 bg-body text-dark">      
+                    <form method='POST' enctype=multipart/form-data action="user-save-changes.php">
+                    <div class="card form-control" >
+                        <h3> <?php echo "Welcome " . $pseudo;   ?></h3>
+                        <?php  echo '<img id="avatarDisplay" src="'.$avatar.'" alt="avatar"><br>'; ?>    
+                        <div class="card-body">
+                        <input class="form-control" id=avatar type='file' name='file'/>
+                        </div>
+                    </div>              
+                    <label for="oldPass">Old password</label><br>
+                    <input type="password" name="oldPass"><br>
+                    <label for="newPass">New password</label><br>
+                    <input type="password" name="newPass"><br>
+                    <label for="newPass2">New password confirmation</label><br>
+                    <input type="password" name="newPass2"><br>
+                    <div class="d-grid gap-2 mt-3">    
+                    <input class="btn btn-outline-primary btn-block"  type="submit" value="Save Changes"><br>
+                    </div> 
+                    <div class="form-text"><?php echo changesMess(); ?></div>
+                    </form>
+                </div>
+            </div>
+            <?php
+                if(isset($_SESSION['admin'])) {
+                    if($_SESSION['admin'] == 1) {
+                        echo "
+                            </div>
+                            </div>
+                            <div class='tab-pane active' id='admin' role='tabpanel'>                
+                        ";
+                        include "../users/connect-to-bdd.php"; // open database
+                        include "../key.php"; // load API key
+                        include "../admin/adminPanel.php";
+                        echo "
+                            </div>
+                        ";
+                    }
+                }
             ?>
           </div>
         </div>
