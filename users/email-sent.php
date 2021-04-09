b4eaa20f7592aaf43d3bff1de8f3552ee02a7426
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-09 15:28:45 +0200

    Fix emailsent.php

--- users/email-sent.php
+++ users/email-sent.php
@@ -10,7 +10,7 @@
         <div class="container">
             <div class="row my-5 d-flex justify-content-center">
                 <div class="col-auto border border-white border-1 rounded shadow p-3 mb-5 mt-5">
-                    <h4>Un email a été envoyé à l'adresse <?php echo $_GET['email']  ?> . <br> Veuillez vérifier votre boite mail. </h4>
+                    <h4>Un email a été envoyé à l'adresse <?php echo $_GET['email']; ?>.<br>Veuillez vérifier votre boite mail. </h4>
                 </div>
             </div>
         </div>
