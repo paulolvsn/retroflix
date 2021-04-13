f765c25747b2034080f7ced2bcd592fc4387d7ec
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-13 07:12:36 +0000

    Update BDD.php credentials

--- users/connect-to-bdd.php
+++ users/connect-to-bdd.php
@@ -1,8 +1,9 @@
 <?php
     try
     {
-        $bdd = new PDO('mysql:host=database;dbname=retroflix;charset=utf8', 'root', 'root');
+        // $bdd = new PDO('mysql:host=database;dbname=retroflix;charset=utf8', 'root', 'root');
         // $bdd = new PDO('mysql:host=localhost;dbname=id16499719_retroflix;charset=utf8', 'id16499719_admin', 'fJC(C?u+<x85+b-M');
+	$bdd = new PDO('mysql:host=localhost;dbname=id16499719_retroflix;charset=utf8', 'root', 'TcmhP*2lqWew$GH2@nbC');
     }
     catch(Exception $e)
     {
