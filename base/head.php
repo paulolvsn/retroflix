648710900238d49f75ba88bc54aafbbdfc001b98
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-15 01:01:09 +0200

    Add preload to head mailchimp

--- base/head.php
+++ base/head.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="preload" type="text/css">
     <link href="/style.css" rel="stylesheet">
     <style type="text/css">
 	    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
